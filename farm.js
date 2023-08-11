c72dd0ce9e3519c80f1e1e4fe009f8de6f616cf9
John Hutcheon <johnhutcheon@johns-mbp.mynet>
2023-08-11 11:53:35 +0100

    completed stage 2 with testing

--- farm.js
+++ farm.js
@@ -1,12 +1,32 @@
 // Level 1
-function cropWateringCalculator(forecast){
-  
+function cropWateringCalculator(forecast) {
+  let count = 0;
+
+  console.log(forecast);
+
+  forecast.forEach((day) => {
+    if (day.weather === "rain") {
+      count++;
+    }
+  });
+
+  return count;
 }
 
 // Level 2 and 3
-function cropWateringCalculatorImproved(forecast, numOfCrops){
+function cropWateringCalculatorImproved(forecast, numOfCrops) {
+  let rainyDays = 0;
 
+  forecast.forEach((day) => {
+    if (day.weather === "rain") {
+      rainyDays++;
+    }
+  });
+
+  if (rainyDays === 1) {
+    return `There is ${rainyDays} day that you can skip watering your crops. You will need ${numOfCrops} sprinkles of water.`;
+  } else
+    return `There are ${rainyDays} days that you can skip watering your crops. You will need ${numOfCrops} sprinkles of water.`;
 }
 
-module.exports = { cropWateringCalculator, cropWateringCalculatorImproved }
-
+module.exports = { cropWateringCalculator, cropWateringCalculatorImproved };
--- farm.test.js
+++ farm.test.js
@@ -1,1 +1,61 @@
-// your code here
+const {
+  cropWateringCalculator,
+  cropWateringCalculatorImproved,
+} = require("./farm");
+
+const { mixedForecast, noRainForecast } = require("./resources/weather");
+
+describe("cropWateringCalculator", () => {
+  test("function takes an array of weather forecast objects and returns a number ", () => {
+    const result = cropWateringCalculator([]);
+    expect(typeof result).toEqual("number");
+  });
+  test("function returns correct number of 0 when no rainy days are forecast", () => {
+    const result = cropWateringCalculator(noRainForecast);
+    expect(result).toEqual(0);
+  });
+  test("function returns correct number when one rainy day is forecast", () => {
+    const result = cropWateringCalculator([{ day: 1, weather: "rain" }]);
+    expect(result).toEqual(1);
+  });
+  test("function returns correct number when passed multiple rainy and sunny days", () => {
+    const result = cropWateringCalculator(mixedForecast);
+    expect(result).toEqual(7);
+  });
+});
+
+describe("cropWateringCalculatorImproved", () => {
+  test("function returns a string", () => {
+    const result = cropWateringCalculatorImproved(
+      [{ day: 1, weather: "sunny" }],
+      2
+    );
+    expect(typeof result).toEqual("string");
+  });
+
+  test("function takes a forecast for one sunny day and returns the correct string", () => {
+    const result = cropWateringCalculatorImproved(
+      [{ day: 1, weather: "sunny" }],
+      2
+    );
+    expect(result).toEqual(
+      "There are 0 days that you can skip watering your crops. You will need 2 sprinkles of water."
+    );
+  });
+
+  test("function takes a forecast for one rainy day and returns the correct string", () => {
+    const result = cropWateringCalculatorImproved(
+      [{ day: 1, weather: "rain" }],
+      2
+    );
+    expect(result).toEqual(
+      "There is 1 day that you can skip watering your crops. You will need 2 sprinkles of water."
+    );
+  });
+  test("function takes a mixed forecast and returns the correct string", () => {
+    const result = cropWateringCalculatorImproved(mixedForecast, 115);
+    expect(result).toEqual(
+      "There are 7 days that you can skip watering your crops. You will need 115 sprinkles of water."
+    );
+  });
+});
